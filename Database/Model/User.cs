4ab579a09362dea4a99a32c6963a08ac721b568b
nazzmullxd <mdnazmul723048@gmail.com>
2025-08-03 00:31:39 +0600

    Added Strucuture for Repository and Interface
    Updated User Model

--- Database/Model/User.cs
+++ Database/Model/User.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Database.Model
 {
+    public enum UserRole
+    {
+        Admin,
+        Employee
+    }
     public class User
     {
+        [Key]
+        public string UserId { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(255)]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(255)]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(20)]
+        public UserRole Role { get; set; } = UserRole.Employee;
     }
 }
